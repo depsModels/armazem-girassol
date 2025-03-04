64404698058de005b9c60bdd96b78f2a34ea0883
MiguelLewandowski <miguelstrapazonl@gmail.com>
2025-03-04 12:43:54 -0300

    :wastebasket: excluir arquivos docker que nao sao utilizados

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,20 +2,52 @@ import Header from "@/components/header";
 import "./globals.css";
 import { CartProvider } from "@/context/CartContext";
 import Footer from "@/components/footer";
+import Script from "next/script";
+
+<Script
+  type="application/ld+json"
+  dangerouslySetInnerHTML={{
+    __html: JSON.stringify({
+      "@context": "https://schema.org",
+      "@type": "Store",
+      "name": "Armazém Girassol",
+      "url": "https://www.armazemgirassol.com.br",
+      "logo": "https://www.armazemgirassol.com.br/logo.png",
+      "description": "Loja online de produtos naturais a granel, saudáveis e orgânicos.",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "Av. Cruz de Malta, 1402 - loja 1",
+        "addressLocality": "Charqueadas",
+        "addressRegion": "RS",
+        "postalCode": "96745-000",
+        "addressCountry": "BR"
+      },
+    }),
+  }}
+/>
 
 export const metadata = {
-  title: "Armazem Girassol",
-  description: "E-commerce gerado por Miguel Lewandowski",
+  title: "Armazém Girassol - Loja de Produtos Naturais a Granel",
+  description:
+    "Compre produtos naturais a granel no Armazém Girassol. Alimentos saudáveis, orgânicos e sustentáveis com entrega rápida e preços acessíveis.",
+  keywords:
+    "armazém girassol, loja de produtos naturais, produtos naturais a granel, alimentos saudáveis, orgânicos, loja online de produtos naturais",
+  openGraph: {
+    title: "Armazém Girassol - Loja de Produtos Naturais a Granel",
+    description:
+      "Explore nossa variedade de produtos naturais a granel com qualidade e preço justo. Entrega rápida e sustentável.",
+    url: "https://www.armazemgirassol.com.br",
+    siteName: "Armazém Girassol",
+    type: "website",
+  },
   icons: {
     icon: [
-      { url: '/favicon.ico' },
-      { url: '/favicon.ico', sizes: '32x32' },
-      { url: '/favicon.ico', sizes: '16x16' },
+      { url: "/favicon.ico" },
+      { url: "/favicon.ico", sizes: "32x32" },
+      { url: "/favicon.ico", sizes: "16x16" },
     ],
-    shortcut: ['/favicon.ico'],
-    apple: [
-      { url: '/favicon.ico' },
-    ],
+    shortcut: ["/favicon.ico"],
+    apple: [{ url: "/favicon.ico" }],
   },
 };
 
@@ -23,6 +55,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="pt-BR">
       <head>
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Armazém Girassol" />
+        <meta property="og:locale" content="pt_BR" />
+        <link rel="canonical" href="https://www.armazemgirassol.com.br" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body>
